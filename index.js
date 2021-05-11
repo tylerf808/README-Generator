f26217962cf565d97b1c982aabb542d14268e2f3
Tyler Fabian <tylerf808@gmail.com>
2021-05-11 16:25:13 -0400

    Added questions to questions array

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your name?', 'What is the title of the project?', 'Provide a description for the project: ',
+    'Provide installation instructions: ', 'Provide usage information: ', 'Provide contribution guidelines: ', 'Provide testing instructions: '
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
